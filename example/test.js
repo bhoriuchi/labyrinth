9eef837801ce8639c446eca2dd9367cdfb09158a
Branden Horiuchi <bhoriuchi@gmail.com>
2015-10-05 15:13:58 -0700

    working on new workflow

--- example/test.js
+++ example/test.js
@@ -30,11 +30,7 @@ var labyrinth = dream.register.plugin('labyrinth', require('../lib/labyrinth'));
 var _         = dream.mods.lodash;
 
 labyrinth.modules.set(['console', 'lodash']);
-dream.register.middleware(function(req, res, next) {
-	console.log('running middleware for ', req.url);
-	req.url = (req.url === '/public') ? '/public/index.html' : req.url;
-	return next();
-});
+
 
 labyrinth.setup.install().then(function() {
 	//console.log('Created routes', _.uniq(_.pluck(labyrinth.routes, 'route.path')));
@@ -44,7 +40,7 @@ labyrinth.setup.install().then(function() {
 	routes.push(dream.staticRoute({
 		path: /\/public\/?.*/,
 		directory: __dirname.replace('/example', ''),
-		'default': '/index.html'
+		'default': 'index.html'
 	}));
 	
 	dream.run(routes);
